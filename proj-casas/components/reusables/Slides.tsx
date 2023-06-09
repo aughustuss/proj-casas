3cc0284c97e0faacf094aeb739783c9ff5703fcd
augusto <augusdpmenezes@gmail.com>
2023-06-09 13:24:27 -0300

    adição de setas aos slides

--- proj-casas/components/reusables/Slides.tsx
+++ proj-casas/components/reusables/Slides.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { Pagination } from 'swiper'
+import React, { useRef, useState } from 'react'
+import { Navigation } from 'swiper'
 import { Swiper } from 'swiper/react'
-import "swiper/css";
-import "swiper/css/pagination"
-
+import "swiper/css"
+import "swiper/css/navigation";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 interface SlidesProps {
     children: any,
     classes: string,
 }
 
 const Slides = ({ children, classes }: SlidesProps) => {
+    const [_, setInit] = useState<boolean>(false);
+    const nextRef = useRef(null);
+    const prevRef = useRef(null);
     return (
         <>
             <Swiper
-                modules={[Pagination]}
+                modules={[Navigation]}
                 autoplay
+                navigation={{
+                    nextEl: nextRef.current,
+                    prevEl: prevRef.current,
+                }}
+                onInit={() => setInit(true)}
                 breakpoints={
                     {
                         0: {
@@ -39,9 +47,11 @@ const Slides = ({ children, classes }: SlidesProps) => {
                         },
                     }
                 }
-                className={classes}
+                className={`relative ${classes}`}
             >
                 {children}
+                <div className='absolute left-0 top-[40%] cursor-pointer z-10 p-[2px] bg-primary rounded-full text-white font-semibold' ref={prevRef}><MdKeyboardArrowLeft size={30}/></div>
+                <div className='absolute right-0 top-[40%] z-10 cursor-pointer p-[2px] bg-primary rounded-full text-white ' ref={nextRef}><MdKeyboardArrowRight size={30}/></div>
             </Swiper>
         </>
     )
